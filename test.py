90967dc54ebd88aa86353b150fa6563013692a08
宇杰 王 <FkerYJ@outlook.com>
2023-05-10 01:29:34 +0800

    fix

--- test.py
+++ test.py
@@ -5,5 +5,9 @@ parser = Parser()
 input = open('test/example.pas').read()
 data = parser.run(input)
 result = code_generate(data[0], data[1])
+with open("test/example.ast","w+") as fw:
+  fw.write(data[0])
+with open("test/example.symTable","w+") as fw:
+  fw.write(data[1])
 with open("test/example.c","w+") as fw:
   fw.write(result)
