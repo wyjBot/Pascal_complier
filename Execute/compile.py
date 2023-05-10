16182f920191564fa06553ad2e29b12bc099496d
宇杰 王 <FkerYJ@outlook.com>
2023-05-10 11:24:00 +0800

    execute module test

--- Execute/compile.py
+++ Execute/compile.py
@@ -11,31 +11,36 @@ import json as js
 
 parser = Parser()
 
-def compile(pasPth,cPth=None,astPth=None,genAstFil=True,showDebugInf=True):
+def compile(pasPth,cPth=None,astPth=None,errPth=None,genAstFil=True,showDebugInf=True):
   src = open(pasPth).read()
   if not cPth:
     cPth=pth.join(pth.dirname(pasPth),pth.basename(pasPth).split('.')[0]+'.c')
     if showDebugInf: print("set default cPth to",cPth)
   if genAstFil and not astPth:
     astPth=pth.join(pth.dirname(pasPth),pth.basename(pasPth).split('.')[0]+'.ast')
-  errPth=pth.join(pth.dirname(pasPth),pth.basename(pasPth).split('.')[0]+'.err')
+    if showDebugInf: print("set default astPth to",astPth)
+  if not errPth:
+    errPth=pth.join(pth.dirname(pasPth),pth.basename(pasPth).split('.')[0]+'.err')
+    if showDebugInf: print("set default errPth to",errPth)
   ast,symb,warn,err = parser.run(src)
   if showDebugInf and (warn):
     print(warn)
-  if showDebugInf and (err):
-    print(err)
   result = code_generate(ast,symb)
   if genAstFil:
     with open(astPth,"w+") as fw:
       js.dump(astPth,fw,indent=2)
   if err:
+    print(err)
     with open(errPth,"w+") as fw:
       js.dump(err,fw,indent=2)
     return False,err
     #err,abort
   else:
-    with open(cPth,"w+") as fw:
-      fw.write(result)
+    if pth.exists(errPth):os.remove(errPth)
+  with open(cPth,"w+") as fw:
+    fw.write(result)
+  if showDebugInf :
+    print('+ ',pasPth,"compile complete!")
   #All information writed,Finally return
   return True,ast,result,warn
 
--- Execute/execute.py
+++ Execute/execute.py
@@ -7,19 +7,29 @@ sys.path.append(cwd)
 from cfg import *
 def execute(codePth,inPth,outPth=None,binPth=None,timeout=3):
   name=pth.basename(codePth).split('.')[0]
-  if not binPth: binPth=pth.join(pwd,f"{name}.run")
+  if not binPth: 
+    # No suffix required, compatible with Windows and Linux
+    binPth=pth.join(pth.dirname(codePth),pth.basename(codePth).split('.')[0])
   cmd1=f"{gccPth} {codePth} -o {binPth}" # print(cmd1)
   p1=subproc.Popen(cmd1, shell=True,
               stdin=subprocess.PIPE,
               stdout=subprocess.PIPE,
               stderr=subprocess.PIPE
     )
-  binErr=(p1.stdout.read()+b'\n'+p1.stderr.read()).decode()
-  p1=subproc.Popen(cmd1, shell=True,
+  p1.wait()
+  binErr=(p1.stdout.read()+b'\n'+p1.stderr.read()).decode().strip()
+  if binErr:print('- 中间代码->机器码 错误',binErr)
+  else: print('+ 机器码生成',binPth)
+  p2=subproc.Popen(binPth, shell=True,
               stdin=subprocess.PIPE,
               stdout=subprocess.PIPE,
               stderr=subprocess.PIPE
     )
+  # p2.stdin.write(open(inPth).read().decode())
+  p2.stdin.write("36 99".encode())
+  p2.wait(timeout=timeout)
+  print(p2.stdout.read())
+    
 
 if __name__=="__main__":
-  execute("Data/example.c","./Data/example.in","Data/example.out")
+  execute(cwd+"/Data/example.c",cwd+"/Data/example.in",cwd+"/Data/example.out")
